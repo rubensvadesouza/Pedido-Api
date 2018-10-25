0870221f4e299fb502b7d83115e97264b25d3ce5
rubensvadesouza <rubensvadesouza@hotmail.com>
2018-10-25 16:32:22 -0300

    Alterações

--- Pedido/Startup.cs
+++ Pedido/Startup.cs
@@ -29,15 +29,6 @@ namespace item_api
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-            else
-            {
-                app.UseHsts();
-            }
-
             app.UseSwagger();
 
             // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.),
